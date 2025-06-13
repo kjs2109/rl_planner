c220eb005dc8f3f4e9b1ae63af60558e14fd79c1
kjs2109 <kimjusung2109@naver.com>
2025-06-13 16:46:41 +0900

    update: set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 source ./rl_env/bin/activate
 source ./install/setup.bash
-export PYTHONPATH=$PYTHONPATH:/home/k/my_work/rl_planner/src
-export PYTHONPATH=$PYTHONPATH:/home/k/my_work/rl_planner/ros/rl_planning_simulator/rl_planning_simulator
-export PYTHONPATH=$PYTHONPATH:/home/k/my_work/rl_env/lib/python3.10/site-packages
-export QT_QPA_PLATFORM_PLUGIN_PATH=/home/k/my_work/rl_planner/rl_env/lib/python3.10/site-packages/cv2/qt/plugins
+export PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/src
+export PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/ros/rl_planning_simulator/rl_planning_simulator
+export PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/rl_env/lib/python3.10/site-packages
+export QT_QPA_PLATFORM_PLUGIN_PATH=$SCRIPT_DIR/rl_env/lib/python3.10/site-packages/cv2/qt/plugins
